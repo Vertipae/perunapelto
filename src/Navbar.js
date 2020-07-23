47e47a78cb42d648e5a815103393d878d62c4cce
Vertipae <33002993+Vertipae@users.noreply.github.com>
2020-07-23 17:39:42 +0300

    Melkein valmis

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,21 @@
 import React from "react"
 
 export default () => {
-  return <div className='palikka'>Navbar</div>
+  return (
+    <div className='jumala'>
+      <nav className='navbar'>
+        <div className='containerNav'>
+          <p>Logo</p>
+
+          <ul className='navLinks'>
+            <li>Etusivu</li>
+            <li>Lajikkeet</li>
+            <li>Ota yhteyttä</li>
+          </ul>
+
+          <p>Button</p>
+        </div>
+      </nav>
+    </div>
+  )
 }
