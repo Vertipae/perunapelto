661416c95198e95035523bfb2e6e63692e9f9478
Vertipae <33002993+Vertipae@users.noreply.github.com>
2020-07-24 10:49:09 +0300

    Refactoring app.css navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,20 +2,16 @@ import React from "react"
 
 export default () => {
   return (
-    <div className='jumala'>
-      <nav className='navbar'>
-        <div className='containerNav'>
-          <p>Logo</p>
+    <nav>
+      <p>Logo</p>
 
-          <ul className='navLinks'>
-            <li>Etusivu</li>
-            <li>Lajikkeet</li>
-            <li>Ota yhteyttä</li>
-          </ul>
+      <ul className='navLinks'>
+        <li>Etusivu</li>
+        <li>Lajikkeet</li>
+        <li>Ota yhteyttä</li>
+      </ul>
 
-          <p>Button</p>
-        </div>
-      </nav>
-    </div>
+      <p>Button</p>
+    </nav>
   )
 }
