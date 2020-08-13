9aa58cb2508a5888529b33cdbbea4000de16c80e
Vertipae <33002993+Vertipae@users.noreply.github.com>
2020-08-13 20:57:51 +0300

    Updates

---
+++ precache-manifest.06da4896a96a98c59db51edf074beaea.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25c069ad4fb832628594a2d12515fedb",
+    "url": "/perunapelto/index.html"
+  },
+  {
+    "revision": "b6314b98a668b223a01f",
+    "url": "/perunapelto/static/css/main.e2487774.chunk.css"
+  },
+  {
+    "revision": "3294958b5c9097b6e525",
+    "url": "/perunapelto/static/js/2.96bd4d29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/perunapelto/static/js/2.96bd4d29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6314b98a668b223a01f",
+    "url": "/perunapelto/static/js/main.fc36c875.chunk.js"
+  },
+  {
+    "revision": "6cea4dfd2e14ab3b20d0",
+    "url": "/perunapelto/static/js/runtime-main.91f91d6e.js"
+  },
+  {
+    "revision": "2d3dde6ab6736032a6b6013845ccc4de",
+    "url": "/perunapelto/static/media/background.2d3dde6a.jpg"
+  },
+  {
+    "revision": "884003b02b7b628bcceb626c30bc8ce7",
+    "url": "/perunapelto/static/media/h1.884003b0.jpg"
+  },
+  {
+    "revision": "5eb45aa43b84e291ae62c9ca344c30a4",
+    "url": "/perunapelto/static/media/h2.5eb45aa4.jpg"
+  },
+  {
+    "revision": "e62010649c50cde06679b623719cabf7",
+    "url": "/perunapelto/static/media/h3.e6201064.jpg"
+  }
+]);
